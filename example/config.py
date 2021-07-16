8ba43eaf0e346a538e62c441c98cdeea01a66bf6
Alessandra Bilardi <alessandra.bilardi@pixartprinting.com>
2021-07-16 08:15:47 +0200

    step 5 - add infrastructure and example configuration

---
+++ example/config.py
@@ -0,0 +1,425 @@
+import os
+import git
+from datetime import datetime
+
+import boto3
+import sagemaker
+if os.path.isdir('/Users') or os.path.isdir('/home/jovyan/'):
+    boto3.setup_default_session(profile_name='your-account', region_name='eu-west-1')
+else:
+    boto3.setup_default_session()
+
+def get_git_details():
+    repo = git.Repo(search_parent_directories=True)
+    return [ repo.active_branch.name, repo.head.object.hexsha ]
+def create_ts():
+    now = datetime.now()
+    return now.strftime('%Y-%m-%d-%H-%M-%S')
+def slash_to_dash(string):
+    return string.replace('/','-')
+def dictionary_from_module(module):
+    context = {}
+    black_list = ['os', 'git', 'datetime', 'boto3', 'sagemaker', 'get_git_details', 'create_ts', 'slash_to_dash', 'dictionary_from_module']
+    for setting in dir(module):
+        # you can write your filter here
+        if not setting.startswith('_') and not setting in (black_list):
+            context[setting] = getattr(module, setting)
+    return context
+
+# common input
+service = 'mlops' # name of your service
+environment = 'studio' # stanging / production
+ecr_repository_name = f'{service}-{environment}-processing'
+[ branch, commit ] = get_git_details()
+#branch = 'alessandra'
+#commit = 'bc7ed76e07967efaf3993b437a2d65b3ce28e19c'
+ts = create_ts()
+
+# environment details of Step Functions execution
+execution_id = ''
+#execution_id = 'arn:aws:states:eu-west-1:1234567890:execution:mlops-studio-prediction:mlops-studio-prediction-2021-07-14T15-58-52193906'
+if os.environ.get('ExecutionId'):
+    execution_id = os.environ.get('ExecutionId')
+execution_name = ''
+#execution_name = 'mlops-studio-prediction-2021-07-14T15-58-52193906'
+if os.environ.get('ExecutionName'):
+    execution_name = os.environ.get('ExecutionName')
+
+#source_bucket = 'your-account'
+source_bucket = sagemaker.Session().default_bucket()
+model_bucket = source_bucket # it is different, if you want to define an expiration date for old models
+destination_bucket = source_bucket # it is different, if you want to define a trigger for your predictions / reports
+key = f'{service}/{branch}/{commit}/{ts}' # for testing
+#key=f'{service}/{branch}/{environment}/{commit}/{ts}' for CD
+if os.environ.get('KEY'):
+    key = os.environ.get('KEY')
+dash_key = slash_to_dash(key)
+test_key = key # it is different, if you want to try modeling without to run again pretraining
+
+# you can define statically some parameters
+#test_key='mlops/alessandra/bc7ed76e07967efaf3993b437a2d65b3ce28e19c/2021-07-14-09-23-20'
+#BestTrainingJob = {'TrainingJobName': 'mlopsstudio20210714T09235703702-008-5e5c0ba9'}
+#model_input_id='mlopsstudio20210714t09235703702'
+#role_arn=''
+#processing_image_uri=''
+#container_image_uri=''
+
+# path of your data
+raw_data_filename='winequality-red.csv'
+raw_data_key=f'{service}/{branch}/raw_data' # for testing
+#raw_data_key=f'{key}/raw_data' # for production
+raw_data_s3_url=f's3://{source_bucket}/{raw_data_key}/{raw_data_filename}'
+
+new_data_filename='winequality-white.csv'
+new_data_key=f'{service}/{branch}/new_data' # for testing
+#new_data_key=f'{key}/new_data' # for production
+new_data_path=f's3://{source_bucket}/{new_data_key}'
+new_data_s3_url=f'{new_data_path}/{new_data_filename}'
+
+target='quality'
+identifier='id'
+score='score'
+test_size=0.3
+validation_size=0.2
+
+test_filename='test.csv'
+test_data_key=f'{test_key}/test_data'
+testing_data_key=f'{test_key}/testing_data'
+test_path=f's3://{source_bucket}/{test_data_key}'
+test_s3_url=f'{test_path}/{test_filename}'
+
+train_filename='train.csv'
+train_path=f's3://{source_bucket}/{test_key}/train_data'
+train_s3_url=f'{train_path}/{train_filename}'
+
+validation_filename='validation.csv'
+validation_data_key=f'{test_key}/validation_data'
+validation_path=f's3://{source_bucket}/{validation_data_key}'
+validation_s3_url=f'{validation_path}/{validation_filename}'
+
+models_path=f's3://{model_bucket}/{key}/models'
+
+#score_filename='.csv.out'
+score_path=f's3://{source_bucket}/{key}/prediction'
+#score_s3_url=f'{score_path}/{score_filename}'
+
+prediction_filename='prediction.csv'
+# prediction_path=score_path
+prediction_path=f's3://{destination_bucket}/{key}/prediction'
+report_filename='report.csv'
+report_path=prediction_path
+
+# input for images and containers
+container_input = {
+    'framework':'xgboost',
+    'version':'latest',
+    'InstanceCount': 1,
+    'InstanceType': 'ml.m5.large',
+    'VolumeSizeInGB': 10
+}
+
+processing_input = {
+    'InstanceCount': 1,
+    'InstanceType': 'ml.t3.large',
+    'VolumeSizeInGB': 10
+}
+
+# input for processing
+auto_input = {
+    'arguments': [
+        '--repo-url', 'https://github.com/bilardi/aws-mlops.git',
+        '--repo-name', 'aws-mlops',
+        '--repo-branch', branch,
+        '--config-path', 'example.config',
+        '--config-item', 'auto_input',
+        '--class-path', 'example.auto_with_pandas',
+        '--class-name', 'AutoWithPandas',
+        '--key', key
+    ],
+    'inputs': [
+        {
+            'InputName': raw_data_filename,
+            'S3Input': {
+                'LocalPath': '/opt/ml/processing/input',
+                'S3Uri': raw_data_s3_url,
+                'S3InputMode': 'File',
+                'S3DataType': 'S3Prefix'
+            }
+        }
+    ],
+    'outputs': [
+        {
+            'OutputName': train_filename,
+            'S3Output': { 
+                'LocalPath': '/opt/ml/processing/train',
+                'S3Uri': train_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        },
+        {
+            'OutputName': test_filename,
+            'S3Output': { 
+                'LocalPath': '/opt/ml/processing/test',
+                'S3Uri': test_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        }        
+    ]
+}
+pretraining_input = {
+    'arguments': [
+        '--repo-url', 'https://github.com/bilardi/aws-mlops.git',
+        '--repo-name', 'aws-mlops',
+        '--repo-branch', branch,
+        '--config-path', 'example.config',
+        '--config-item', 'pretraining_input',
+        '--class-path', 'example.prep_with_pandas',
+        '--class-name', 'PrepWithPandas',
+        '--key', key
+    ],
+    'inputs': [
+        {
+            'InputName': raw_data_filename,
+            'S3Input': {
+                'LocalPath': '/opt/ml/processing/input',
+                'S3Uri': raw_data_s3_url,
+                'S3InputMode': 'File',
+                'S3DataType': 'S3Prefix'
+            }
+        }
+    ],
+    'outputs': [
+        {
+            'OutputName': train_filename,
+            'S3Output': { 
+                'LocalPath': '/opt/ml/processing/train',
+                'S3Uri': train_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        },
+        {
+            'OutputName': validation_filename,
+            'S3Output': { 
+                'LocalPath': '/opt/ml/processing/validation',
+                'S3Uri': validation_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        },
+        {
+            'OutputName': test_filename,
+            'S3Output': { 
+                'LocalPath': '/opt/ml/processing/test',
+                'S3Uri': test_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        }
+    ]
+}
+preinference_input = {
+    'arguments': [
+        '--repo-url', 'https://github.com/bilardi/aws-mlops.git',
+        '--repo-name', 'aws-mlops',
+        '--repo-branch', branch,
+        '--config-path', 'example.config',
+        '--config-item', 'preinference_input',
+        '--class-path', 'example.prep_with_pandas',
+        '--class-name', 'PrepWithPandas',
+        '--key', key
+    ],
+    'inputs': [
+        {
+            'InputName': new_data_filename,
+            'S3Input': {
+                'LocalPath': '/opt/ml/processing/input',
+                'S3Uri': new_data_s3_url,
+                'S3InputMode': 'File',
+                'S3DataType': 'S3Prefix'
+            }
+        }
+    ],
+    'outputs': [
+        {
+            'OutputName': test_filename,
+            'S3Output': {
+                'LocalPath': '/opt/ml/processing/test',
+                'S3Uri': test_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        }
+    ]
+}
+
+# input for modeling
+tuning_input = {
+    'max_runtime_in_seconds': 86400,
+    'static_hyper_parameters': {
+        '_kfold': '5',
+        '_num_cv_round': '3',
+        'gamma': '1.6816270262197224e-06',
+        'lambda': '0.009173719287637053',
+        'min_child_weight': '0.006254071319537053',
+        'objective': 'reg:linear',
+        'subsample': '0.6058771210998253',
+        'eval_metric':'rmse'
+    },
+    'hyper_parameter_tuning': {
+        'Strategy': 'Bayesian',
+        'ResourceLimits': {
+            'MaxNumberOfTrainingJobs': 10, #250,
+            'MaxParallelTrainingJobs': 10
+        },
+        'TrainingJobEarlyStoppingType': 'Auto',
+        'HyperParameterTuningJobObjective': {
+            'Type': 'Minimize',
+            'MetricName': 'validation:rmse'
+        },
+        'ParameterRanges': {
+            'ContinuousParameterRanges': [
+                {
+                    'Name': 'eta',
+                    'MinValue': '0.02',
+                    'MaxValue': '0.04',
+                    'ScalingType': 'Auto'
+                },
+                {
+                    'Name': 'colsample_bytree',
+                    'MinValue': '0.5',
+                    'MaxValue': '0.8',
+                    'ScalingType': 'Auto'
+                },
+                {
+                    'Name': 'alpha',
+                    'MinValue': '1',
+                    'MaxValue': '3',
+                    'ScalingType': 'Auto'
+                }
+            ],
+            'CategoricalParameterRanges': [],
+            'IntegerParameterRanges': [
+                {
+                    'Name': 'num_round',
+                    'MinValue': '400',
+                    'MaxValue': '800',
+                    'ScalingType': 'Auto'
+                },
+                {
+                    'Name': 'max_depth',
+                    'MinValue': '2',
+                    'MaxValue': '10',
+                    'ScalingType': 'Auto'
+                }
+            ]
+        }
+    }
+}
+training_input = {
+    'static_hyper_parameters': {
+        '_kfold': '5',
+        '_num_cv_round': '3',
+        'alpha': '2.519849147096237e-05',
+        'colsample_bytree': '0.6509641408101212',
+        'eta': '0.02903202312960693',
+        'gamma': '1.6816270262197224e-06',
+        'lambda': '0.009173719287637053',
+        'max_depth': '2',
+        'min_child_weight': '0.006254071319537053',
+        'num_round': '597',
+        # 'reg:linear', 'reg:logistic', 'binary:logistic', 'binary:logitraw', 'count:poisson', 'multi:softmax', 'multi:softprob', 'rank:pairwise', 'reg:gamma', 'reg:tweedie'
+        'objective': 'reg:linear',
+        'subsample': '0.6058771210998253',
+        'eval_metric':'rmse'
+    }
+}
+
+# input for reporting
+testing_input = {
+    'arguments': [
+        '--repo-url', 'https://github.com/bilardi/aws-mlops.git',
+        '--repo-name', 'aws-mlops',
+        '--repo-branch', branch,
+        '--config-path', 'example.config',
+        '--config-item', 'testing_input',
+        '--class-path', 'example.test_with_pandas',
+        '--class-name', 'TestWithPandas',
+        '--key', key
+    ],
+    'inputs': [
+        {
+            'InputName': test_filename + '.out',
+            'S3Input': {
+                'LocalPath': '/opt/ml/processing/score',
+                'S3Uri': score_path,
+                'S3InputMode': 'File',
+                'S3DataType': 'S3Prefix'
+            }
+        },
+        {
+            'InputName': test_filename,
+            'S3Input': {
+                'LocalPath': '/opt/ml/processing/test',
+                'S3Uri': test_s3_url,
+                'S3InputMode': 'File',
+                'S3DataType': 'S3Prefix'
+            }
+        }
+    ],
+    'outputs': [
+        {
+            'OutputName': prediction_filename,
+            'S3Output': {
+                'LocalPath': '/opt/ml/processing/prediction',
+                'S3Uri': prediction_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        },
+        {
+            'OutputName': report_filename,
+            'S3Output': {
+                'LocalPath': '/opt/ml/processing/report',
+                'S3Uri': report_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        }
+    ]
+}
+reporting_input = {
+    'arguments': [
+        '--repo-url', 'https://github.com/bilardi/aws-mlops.git',
+        '--repo-name', 'aws-mlops',
+        '--repo-branch', branch,
+        '--config-path', 'example.config',
+        '--config-item', 'reporting_input',
+        '--class-path', 'example.test_with_pandas',
+        '--class-name', 'TestWithPandas',
+        '--key', key
+    ],
+    'inputs': [
+        {
+            'InputName': test_filename + '.out',
+            'S3Input': {
+                'LocalPath': '/opt/ml/processing/score',
+                'S3Uri': score_path,
+                'S3InputMode': 'File',
+                'S3DataType': 'S3Prefix'
+            }
+        }
+    ],
+    'outputs': [
+        {
+            'OutputName': prediction_filename,
+            'S3Output': {
+                'LocalPath': '/opt/ml/processing/prediction',
+                'S3Uri': prediction_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        },
+        {
+            'OutputName': report_filename,
+            'S3Output': {
+                'LocalPath': '/opt/ml/processing/report',
+                'S3Uri': report_path,
+                'S3UploadMode': 'EndOfJob'
+            }
+        }
+    ]
+}
---
+++ example/definitions.py
@@ -0,0 +1,217 @@
+import config as config
+
+service = config.service
+environment = config.environment
+
+# input for preprocessing
+# https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_CreateProcessingJob.html
+pretraining_input = {
+    'AppSpecification': {
+        'ImageUri.$': '$.processing_image_uri',
+        'ContainerArguments.$': '$.pretraining_input.arguments',
+        'ContainerEntrypoint': [ 'python3', '/app/processing.py' ]
+    },
+    'Environment': {
+#        'last_output.$': '$',
+        'ExecutionId.$': '$$.Execution.Id',
+        'ExecutionName.$': '$$.Execution.Name'
+    },
+    'ProcessingResources': {
+        'ClusterConfig': {
+            'InstanceCount.$': "$.processing_input['InstanceCount']",
+            'InstanceType.$': "$.processing_input['InstanceType']",
+            'VolumeSizeInGB.$': "$.processing_input['VolumeSizeInGB']"
+        }
+    },
+    'ProcessingInputs.$': '$.pretraining_input.inputs',
+    'ProcessingOutputConfig': {
+        'Outputs.$': '$.pretraining_input.outputs'
+    },
+    'RoleArn.$': '$.role_arn',
+    'ProcessingJobName.$': '$.pretraining_input_id'
+}
+preinference_input = {
+    'AppSpecification': {
+        'ImageUri.$': '$.processing_image_uri',
+        'ContainerArguments.$': '$.preinference_input.arguments',
+        'ContainerEntrypoint': [ 'python3', '/app/processing.py' ]
+    },
+    'Environment': {
+#        'last_output.$': '$',
+        'ExecutionId.$': '$$.Execution.Id',
+        'ExecutionName.$': '$$.Execution.Name'
+    },
+    'ProcessingResources': {
+        'ClusterConfig': {
+            'InstanceCount.$': "$.processing_input['InstanceCount']",
+            'InstanceType.$': "$.processing_input['InstanceType']",
+            'VolumeSizeInGB.$': "$.processing_input['VolumeSizeInGB']"
+        }
+    },
+    'ProcessingInputs.$': '$.preinference_input.inputs',
+    'ProcessingOutputConfig': {
+        'Outputs.$': '$.preinference_input.outputs'
+    },
+    'RoleArn.$': '$.role_arn',
+    'ProcessingJobName.$': '$.preinference_input_id'
+}
+
+# input for modeling
+# https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_CreateHyperParameterTuningJob.html
+tuning_input = {
+    'TrainingJobDefinition': {
+        'AlgorithmSpecification': {
+            'TrainingImage.$': '$.container_image_uri',
+            'TrainingInputMode': 'File'
+        },
+        'OutputDataConfig': {
+            'S3OutputPath.$': '$.models_path'
+        },
+        'StoppingCondition': {
+            'MaxRuntimeInSeconds.$': '$.tuning_input.max_runtime_in_seconds'
+        },
+        'ResourceConfig': {
+            'InstanceCount.$': "$.container_input['InstanceCount']",
+            'InstanceType.$': "$.container_input['InstanceType']",
+            'VolumeSizeInGB.$': "$.container_input['VolumeSizeInGB']"
+        },
+        'RoleArn.$': '$.role_arn',
+        'InputDataConfig': [
+            {
+                'DataSource': {
+                    'S3DataSource': {
+                        'S3DataType': 'S3Prefix',
+                        'S3Uri.$': '$.train_path',
+                        'S3DataDistributionType': 'FullyReplicated'
+                    }
+                },
+                'ContentType': 'text/csv',
+                'ChannelName': 'train'
+            },
+            {
+                'DataSource': {
+                    'S3DataSource': {
+                        'S3DataType': 'S3Prefix',
+                        'S3Uri.$': '$.validation_path',
+                        'S3DataDistributionType': 'FullyReplicated'
+                    }
+                },
+                'ContentType': 'text/csv',
+                'ChannelName': 'validation'
+            }
+        ],
+        'StaticHyperParameters.$': '$.tuning_input.static_hyper_parameters'
+    },
+    'HyperParameterTuningJobName.$': '$.tuner_input_id',
+    'HyperParameterTuningJobConfig.$': '$.tuning_input.hyper_parameter_tuning' 
+}
+# https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_CreateTrainingJob.html
+training_input = {}
+
+# input for inference
+# https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_CreateModel.html
+model_input = {
+    'ExecutionRoleArn.$': '$.role_arn',
+    'ModelName.$': '$.model_input_id',
+    'PrimaryContainer': {
+        'Environment': {},
+        'Image.$': '$.container_image_uri',
+        'ModelDataUrl.$': "$['S3ModelArtifacts']"
+    }
+}
+# https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_CreateTransformJob.html
+testing_transformer_input = {
+    'TransformJobName.$': '$.testing_transformer_input_id',
+    'ModelName.$': '$.model_input_id',
+    'TransformInput': {
+        'DataSource': {
+            'S3DataSource': {
+                'S3DataType': 'S3Prefix',
+                'S3Uri.$': '$.test_path'
+            }
+        },
+        'ContentType': 'text/csv'
+    },
+    'TransformOutput': {
+        'S3OutputPath.$': '$.prediction_path'
+    },
+    'TransformResources': {
+        'InstanceCount.$': "$.container_input['InstanceCount']",
+        'InstanceType.$': "$.container_input['InstanceType']",
+#        'VolumeSizeInGB.$': "$.container_input['VolumeSizeInGB']"
+    }
+}
+prediction_transformer_input = {
+    'TransformJobName.$': '$.prediction_transformer_input_id',
+    'ModelName.$': '$.model_input_id',
+    'TransformInput': {
+        'DataSource': {
+            'S3DataSource': {
+                'S3DataType': 'S3Prefix',
+                'S3Uri.$': '$.test_path'
+            }
+        },
+        'ContentType': 'text/csv'
+    },
+    'TransformOutput': {
+        'S3OutputPath.$': '$.prediction_path'
+    },
+    'TransformResources': {
+        'InstanceCount.$': "$.container_input['InstanceCount']",
+        'InstanceType.$': "$.container_input['InstanceType']",
+#        'VolumeSizeInGB.$': "$.container_input['VolumeSizeInGB']"
+    }
+}
+
+# input for reporting
+# https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_CreateProcessingJob.html
+testing_input = {
+    'AppSpecification': {
+        'ImageUri.$': '$.processing_image_uri',
+        'ContainerArguments.$': '$.testing_input.arguments',
+        'ContainerEntrypoint': [ 'python3', '/app/processing.py' ]
+    },
+    'Environment': {
+#        'last_output.$': '$',
+        'ExecutionId.$': '$$.Execution.Id',
+        'ExecutionName.$': '$$.Execution.Name'
+    },
+    'ProcessingResources': {
+        'ClusterConfig': {
+            'InstanceCount.$': "$.processing_input['InstanceCount']",
+            'InstanceType.$': "$.processing_input['InstanceType']",
+            'VolumeSizeInGB.$': "$.processing_input['VolumeSizeInGB']"
+        }
+    },
+    'ProcessingInputs.$': '$.testing_input.inputs',
+    'ProcessingOutputConfig': {
+        'Outputs.$': '$.testing_input.outputs'
+    },
+    'RoleArn.$': f'$.role_arn',
+    'ProcessingJobName.$': f'$.testing_input_id'
+}
+reporting_input = {
+    'AppSpecification': {
+        'ImageUri.$': '$.processing_image_uri',
+        'ContainerArguments.$': '$.reporting_input.arguments',
+        'ContainerEntrypoint': [ 'python3', '/app/processing.py' ]
+    },
+    'Environment': {
+#        'last_output.$': '$',
+        'ExecutionId.$': '$$.Execution.Id',
+        'ExecutionName.$': '$$.Execution.Name'
+    },
+    'ProcessingResources': {
+        'ClusterConfig': {
+            'InstanceCount.$': "$.processing_input['InstanceCount']",
+            'InstanceType.$': "$.processing_input['InstanceType']",
+            'VolumeSizeInGB.$': "$.processing_input['VolumeSizeInGB']"
+        }
+    },
+    'ProcessingInputs.$': '$.reporting_input.inputs',
+    'ProcessingOutputConfig': {
+        'Outputs.$': '$.reporting_input.outputs'
+    },
+    'RoleArn.$': f'$.role_arn',
+    'ProcessingJobName.$': f'$.reporting_input_id'
+}
